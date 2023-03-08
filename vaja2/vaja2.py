94078382959d8a18fdac2162f950f6dbd5385ed2
MatejPC <matej113a@gmail.com>
2023-03-08 19:44:41 +0100

    inicializacijski commit

---
+++ vaja2/vaja2.py
@@ -0,0 +1,16 @@
+
+def my_roberts(slika):
+    #vaša implementacija
+    return slika_robov 
+
+def my_prewitt(slika):
+    #vaša implementacija
+    return slika_robov 
+
+def my_sobel(slika):
+    #vaša implementacija
+    return slika_robov 
+
+def canny(slika, sp_prag, zg_prag):
+    #vaša implementacija
+    return slika_robov 
