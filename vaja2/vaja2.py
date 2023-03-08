4907043b1b03c08417185f9ce4b0c5042ee8b7af
MatejPC <matej113a@gmail.com>
2023-03-08 20:56:37 +0100

    osnovna impmentacija in testiranje my_roberts

--- vaja2/vaja2.py
+++ vaja2/vaja2.py
@@ -1,10 +1,27 @@
 import cv2
 import numpy as np
 
-"""
-def my_roberts(slika):
-   return slika_robov 
 
+def my_roberts(slika):
+    # Define the Roberts kernels
+    rx = np.array([[1, 0], [0, -1]], dtype=np.float32)
+    ry = np.array([[0, 1], [-1, 0]], dtype=np.float32)
+
+    # Define the Roberts kernels
+    irx = cv2.filter2D(img, -1, rx)
+    iry = cv2.filter2D(img, -1, ry)
+    # Compute the magnitude of the Roberts edge detection
+    slika_robov = np.sqrt(np.square(irx) + np.square(iry))
+    slika_robov = np.uint8(slika_robov)
+
+    # Display the results
+    cv2.imshow('Original Image', img)
+    cv2.imshow('Roberts Edge Detection', slika_robov)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return slika_robov 
+
+"""
 def my_prewitt(slika):
     return slika_robov 
 
@@ -19,11 +36,8 @@ def spremeni_kontrast(slika, alfa, beta):
 """
 
 img = cv2.imread('C:/Users/MatejPC/Desktop/Sola/2.letnik/4.semester/rv/vaja2/vegeta.jpg', 0)
-cv2.imshow('Original Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+my_roberts(img)
 """
-#my_roberts(img)
 # Define the Roberts kernels
 rx = np.array([[1, 0], [0, -1]], dtype=np.float32)
 ry = np.array([[0, 1], [-1, 0]], dtype=np.float32)
