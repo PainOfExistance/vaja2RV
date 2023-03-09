4ce41644eac1d1990415d8e33a35d3cc0a415afc
MatejPC <matej113a@gmail.com>
2023-03-09 13:17:46 +0100

    popravek pri izrisu robov, boljša detekciaj ter osnovno čiščenje šuma slike

--- vaja2/vaja2.py
+++ vaja2/vaja2.py
@@ -12,36 +12,30 @@ def my_roberts(slika):
     irx = cv2.filter2D(slika, -1, rx)
     iry = cv2.filter2D(slika, -1, ry)
     # Compute the magnitude of the Roberts edge detection
-    slika_robov = np.sqrt(np.square(irx) + np.square(iry))
-    slika_robov = np.uint8(slika_robov)
+    robertsSlika = np.sqrt(np.square(irx) + np.square(iry))
+    robertsSlika = np.uint8(robertsSlika)
 
     alfa = float(input("vnestie alfa za kontrast:"))
     beta = float(input("vnestie beta za kontrast:"))
-    slika_robov = spremeni_kontrast(slika_robov, alfa, beta)
-    
-    # define range of white color in HSV
-    # change it according to your need !
-    lower_white = np.array([50], dtype=np.uint8)
+    robertsSlika = spremeni_kontrast(robertsSlika, alfa, beta)
+
+    lower_white = np.array([30], dtype=np.uint8)
     upper_white = np.array([255], dtype=np.uint8)
 
-    # Threshold the HSV image to get only white colors
-    mask = cv2.inRange(slika_robov, lower_white, upper_white)
-    # Bitwise-AND mask and original image
-    dst = cv2.bitwise_and(slika_robov,slika_robov, mask= mask)
+    mask = cv2.inRange(robertsSlika, lower_white, upper_white)
+    dst = cv2.bitwise_and(robertsSlika, robertsSlika, mask=mask)
 
-    blank_image = np.zeros((slika.shape[0],slika.shape[1],3), np.uint8)
-    finslika = np.zeros((slika.shape[0],slika.shape[1],3), np.uint8)
+    slika_robov = np.zeros((slika.shape[0], slika.shape[1], 3), np.uint8)
+    finslika = np.zeros((slika.shape[0], slika.shape[1], 3), np.uint8)
 
     for i in range(0, slika.shape[0]):
         for j in range(0, slika.shape[1]):
-            blank_image[i,j]=[0, 0, slika_robov[i,j]]
-            finslika[i,j]=[slika[i,j], slika[i,j], slika[i,j]]
-           
-    blank_image = cv2.addWeighted(finslika, 0.4, blank_image, 1.5, 3)
+            slika_robov[i, j] = [0, 0, dst[i, j]]
+            finslika[i, j] = [slika[i, j], slika[i, j], slika[i, j]]
 
+    slika_robov = cv2.addWeighted(finslika, 0.4, slika_robov, 1.5, 3)
 
-    return blank_image
-
+    return slika_robov
 
 """
 def my_prewitt(slika):
@@ -55,7 +49,6 @@ def canny(slika, sp_prag, zg_prag):
 
 """
 
-
 def spremeni_kontrast(slika, alfa, beta):
     for i in range(0, slika.shape[0]):
         for j in range(0, slika.shape[1]):
@@ -63,34 +56,17 @@ def spremeni_kontrast(slika, alfa, beta):
             slika[i, j] = temp
     return slika
 
-
-img = cv2.imread("C:/Users/MatejPC/Desktop/Sola/2.letnik/4.semester/rv/vaja2/vegeta.jpg", 0)
+img = cv2.imread(
+    "C:/Users/MatejPC/Desktop/Sola/2.letnik/4.semester/rv/vaja2/lenna.png", 0
+)
+img = cv2.resize(img, (800, 800))
 alfa = float(input("vnestie alfa za kontrast:"))
 beta = float(input("vnestie beta za kontrast:"))
 img = spremeni_kontrast(img, alfa, beta)
 
 
-finimg=my_roberts(img)
+finimg = my_roberts(img)
 
 cv2.imshow("Edge Detection", finimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-"""
-# Define the Roberts kernels
-rx = np.array([[1, 0], [0, -1]], dtype=np.float32)
-ry = np.array([[0, 1], [-1, 0]], dtype=np.float32)
-
-# Apply the Roberts kernels to the image
-irx = cv2.filter2D(img, -1, rx)
-iry = cv2.filter2D(img, -1, ry)
-
-# Compute the magnitude of the Roberts edge detection
-img_roberts = np.sqrt(np.square(irx) + np.square(iry))
-img_roberts = np.uint8(img_roberts)
-
-# Display the results
-cv2.imshow('Original Image', img)
-cv2.imshow('Roberts Edge Detection', img_roberts)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
