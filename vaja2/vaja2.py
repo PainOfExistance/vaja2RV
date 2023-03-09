3336adbdfebea1b3389f12b121c0572b2dd9da6e
MatejPC <matej113a@gmail.com>
2023-03-09 10:48:23 +0100

    dodana funkcija za spreminjanje kontrasta z uporabo inputa

--- vaja2/vaja2.py
+++ vaja2/vaja2.py
@@ -1,3 +1,4 @@
+﻿from gzip import READ
 import cv2
 import numpy as np
 
@@ -8,18 +9,22 @@ def my_roberts(slika):
     ry = np.array([[0, 1], [-1, 0]], dtype=np.float32)
 
     # Define the Roberts kernels
-    irx = cv2.filter2D(img, -1, rx)
-    iry = cv2.filter2D(img, -1, ry)
+    irx = cv2.filter2D(slika, -1, rx)
+    iry = cv2.filter2D(slika, -1, ry)
     # Compute the magnitude of the Roberts edge detection
     slika_robov = np.sqrt(np.square(irx) + np.square(iry))
     slika_robov = np.uint8(slika_robov)
 
+    alfa = float(input("vnestie alfa za kontrast:"))
+    beta = float(input("vnestie beta za kontrast:"))
+    slika_robov = spremeni_kontrast(slika_robov, alfa, beta)
     # Display the results
-    cv2.imshow('Original Image', img)
-    cv2.imshow('Roberts Edge Detection', slika_robov)
+    cv2.imshow("Original Image", slika)
+    cv2.imshow("Roberts Edge Detection", slika_robov)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    return slika_robov 
+    return slika_robov
+
 
 """
 def my_prewitt(slika):
@@ -31,11 +36,23 @@ def my_sobel(slika):
 def canny(slika, sp_prag, zg_prag):
     return slika_robov 
 
-def spremeni_kontrast(slika, alfa, beta):
-    return 0
 """
 
-img = cv2.imread('C:/Users/MatejPC/Desktop/Sola/2.letnik/4.semester/rv/vaja2/vegeta.jpg', 0)
+
+def spremeni_kontrast(slika, alfa, beta):
+    for i in range(0, slika.shape[0]):
+        for j in range(0, slika.shape[1]):
+            temp = alfa * slika[i, j] + beta
+            slika[i, j] = temp
+    return slika
+
+
+img = cv2.imread("C:/Users/MatejPC/Desktop/Sola/2.letnik/4.semester/rv/vaja2/vegeta.jpg", 0)
+alfa = float(input("vnestie alfa za kontrast:"))
+beta = float(input("vnestie beta za kontrast:"))
+img = spremeni_kontrast(img, alfa, beta)
+
+
 my_roberts(img)
 """
 # Define the Roberts kernels
